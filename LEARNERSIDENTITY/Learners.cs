af9bdbb182b5a666056d8f98f52dfdf0a09064b2
MAMAOLWETHU <thulisiweb04@gmail.com>
2023-01-24 12:42:07 +0200

    Add project files.

---
+++ LEARNERSIDENTITY/Learners.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEARNERSIDENTITY
+{
+    internal class Learners
+    {
+        public string firstname;
+        public string lastname;
+        public string gender;
+        public int age;
+        public char initial;
+
+        public Learners(string firstname, string lastname, string gender, char initial, int age)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.gender = gender;
+            this.age = age;
+            this.initial = initial;
+        }
+    }
+
+
+    }
---
+++ LEARNERSIDENTITY/Program.cs
@@ -0,0 +1,11 @@
+﻿using LEARNERSIDENTITY;
+class Program
+{
+    static void Main (String[] args)
+    {
+        Learners learner1 = new Learners( "Thuli"," Baloyi"," female",'T',24);
+        Console.WriteLine(learner1.firstname+" "+learner1.lastname+" "+learner1.gender+" "+learner1.age+" "+learner1.initial);
+    }
+}
+
+
